c83722fb0f20adc22842a1cefa32e37fac98e289
rawinolivera <oli18001@byui.edu>
2022-09-29 19:12:05 -0500

    las update

--- snippets/navigation.php
+++ snippets/navigation.php
@@ -1,8 +1,8 @@
 <ul>
-    <a href="/phpmotors/views/home.php"><li>Home</li></a>
-    <a href=""><li>Classic</li></a>
-    <a href=""><li>Sports</li></a>
-    <a href=""><li>SUV</li></a>
-    <a href=""><li>Trucks</li></a>
-    <a href=""><li>Used</li></a>
+    <li><a href="/phpmotors/views/home.php">Home</a></li>
+    <li><a href="">Classic</a></li>
+    <li><a href="">Sports</a></li>
+    <li><a href="">SUV</a></li>
+    <li><a href="">Trucks</a></li>
+    <li><a href="">Used</a></li>
 </ul>
