886b55ffd28be2e979652e26af206b3013274268
rawinolivera <oli18001@byui.edu>
2022-10-03 12:08:00 -0500

    connections update

---
+++ libraries/connections.php
@@ -0,0 +1,28 @@
+<?php 
+/*
+*Proxy connection to the phpmotors database
+*/
+
+function phpmotorsConnect(){
+    $server = 'localhost';
+    $dbname = 'phpmors';
+    $username = 'iClient';
+    $password = '7fOvm2TCJIz0vtlB';
+    $dsn = "mysql:host=$server;dbname=$dbname";
+    $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+
+    try{
+        $link = new PDO($dsn, $username, $password, $options);
+        //if(is_object($link)){
+        //    echo 'It worked';
+        //}
+        return $link;
+    }catch(PDOException $e){
+        //echo "It didn't work, error: " . $e->getMessage();
+        header('location: /phpmotors/views/500.php');
+    }
+
+}
+
+phpmotorsConnect();
+?>
---
+++ snippets/main-500.php
@@ -0,0 +1,4 @@
+<main>
+    <h1>Server Error</h1>
+    <p>Sorry our server seems to be experiencing  some technical difficulties. Please check back later.</p>
+</main>
